af638d98ed47b55bfe1d4b730be22a6bd21d3076
HiD-Yoshinaga <70322682+HiD-Yoshinaga@users.noreply.github.com>
2020-09-22 11:50:56 +0900

    rubocop

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
     # nickname {'テスト'}
-    nickname {Faker::Name.last_name}
+    nickname { Faker::Name.last_name }
     # email {'furima@gmail.com'}
-    email {Faker::Internet.free_email}
-    password {"123456a"}
+    email { Faker::Internet.free_email }
+    password { '123456a' }
     # password {Faker::Internet.password(min_length: 6)}
     # password { password }
-    encrypted_password {"123456a"}
+    encrypted_password { '123456a' }
     family_name { '山田' }
     first_name {'太郎'}
     family_name_kana {'ヤマダ'}
