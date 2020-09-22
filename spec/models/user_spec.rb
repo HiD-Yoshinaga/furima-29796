cd232fc6695119367e4c309f308717d1100b377d
HiD-Yoshinaga <70322682+HiD-Yoshinaga@users.noreply.github.com>
2020-09-22 14:50:37 +0900

    修正2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -74,10 +74,22 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include('Encrypted password パスワードには英字と数字の両方を含めて設定してください')
     end
 
+    it 'first_nameが空では登録できないこと' do
+      @user.first_name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
+
     it 'first_nameは漢字・ひらがな・カタカナでなければ登録できないこと' do
       @user.first_name = 'Taro'
       @user.valid?
       expect(@user.errors.full_messages).to include('First name 全角文字を使用してください')
+    end
+
+    it 'family_nameが空では登録できないこと' do
+      @user.family_name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Family name can't be blank")
     end
 
     it 'family_nameは漢字・ひらがな・カタカナでなければ登録できないこと' do
@@ -86,16 +98,34 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include('Family name 全角文字を使用してください')
     end
 
+    it 'first_name_kanaが空では登録できないこと' do
+      @user.first_name_kana = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name kana can't be blank")
+    end
+
     it 'first_name_kanaはカタカナでなければ登録できないこと' do
       @user.first_name_kana = '太郎'
       @user.valid?
       expect(@user.errors.full_messages).to include('First name kana 全角カタカナを使用してください')
     end
 
+    it 'family_name_kanaが空では登録できないこと' do
+      @user.family_name_kana = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Family name kana can't be blank")
+    end
+
     it 'family_name_kanaはカタカナでなければ登録できないこと' do
       @user.family_name_kana = 'ふりま'
       @user.valid?
       expect(@user.errors.full_messages).to include('Family name kana 全角カタカナを使用してください')
+    end
+
+    it 'birthdayが空では登録できないこと' do
+      @user.birthday = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
   end
 end
