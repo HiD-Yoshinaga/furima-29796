01296b14998587afbaf6e21ca2bf178d5a168fd5
HiD-Yoshinaga <70322682+HiD-Yoshinaga@users.noreply.github.com>
2020-09-24 22:13:09 +0900

    ActiveHashとvalidationの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :item do
-    
+    item_info {Faker::Lorem.sentence}
+    association :user
+    association :order
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
