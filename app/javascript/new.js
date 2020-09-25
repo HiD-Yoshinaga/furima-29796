86bed662d5e86f0d0f3d68375754a9cfdb9137b9
HiD-Yoshinaga <70322682+HiD-Yoshinaga@users.noreply.github.com>
2020-09-25 14:39:53 +0900

    コメントアウトの削除

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -4,9 +4,7 @@ function calculation (){
   priceContent.addEventListener("keyup", () => {
     const price = priceContent.value;
     const tax = Math.floor(price * 0.1)
-    // tax.toLocaleString();
     const profit = price - tax
-    // profit.toLocaleString();
     const addTaxPrice = document.getElementById("add-tax-price");
     addTaxPrice.innerHTML = `${tax.toLocaleString()}`;
     const Profit = document.getElementById("profit");
